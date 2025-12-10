db4063441ae32533412b0b919d0a528ab2708da7
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2025-12-10 09:04:53 +0900

    3D設定のセットアップ

--- InvisibleVector/Main.cpp
+++ InvisibleVector/Main.cpp
@@ -8,15 +8,32 @@
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	SetGraphMode(1280, 720, 32); // 画面サイズの調整
+	ChangeWindowMode(true); // ウィンドウモードに設定
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
+	SetDrawScreen(DX_SCREEN_BACK); // 裏画面(バックバッファの設定)
+
+	// 3D設定 -----------------------------------------------------------------
+
+	SetUseBackCulling(TRUE); // 裏画面の設定
+	SetUseZBuffer3D(true); // zバッファの有効化
+	SetWriteZBuffer3D(true); // zバッファへの書き込み許可
+	SetUseLighting(true); // ライトの有効化
+	SetCameraNearFar(0.1f, 1000.0f); // カメラのクリップ距離
+	SetCameraPositionAndTarget_UpVecY(VGet(320.0f, 240.0f, -10.0f), VGet(320.0f, 240.0f, 1.0f)); // カメラの位置と注視点の設定(位置途中視点はデフォルトです)
+
+
+	// ------------------------------------------------------------------------
 
 	// ゲームループの実装
 	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0)
 	{
+
+
 		DrawFormatString(20, 20, GetColor(255, 255, 255), "Hello_World_Debug");
 	}
 
