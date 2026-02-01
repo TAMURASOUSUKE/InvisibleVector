cf6e40c39fe0b4cefec0770dac3c59a88c6f1c23
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-02-01 09:35:38 +0900

    GameKey.hの追加

--- InvisibleVector/Constant/GameConstant.h
+++ InvisibleVector/Constant/GameConstant.h
@@ -4,8 +4,23 @@
 	ゲームシステムに関する定数や汎用的な構造体等を定義する
 */
 
-const int SCREEN_WIDTH = 1280;
-const int SCREEN_HEIGHT = 720;
-const int COLOR_DEPTH = 32;
-const float CAMERA_NEAR = 0.1f;
-const float CAMERA_FAR = 10000.0f;
+const int SCREEN_WIDTH = 1280; // 横幅
+const int SCREEN_HEIGHT = 720; // 縦幅
+const int COLOR_DEPTH = 32; // 色の深度
+const float CAMERA_NEAR = 1.0f; // 視錐台の上面までの距離
+const float CAMERA_FAR = 10000.0f; // 視錐台の底面までの距離
+
+
+/*
+	ゲームで使う入力をまとめた列挙体
+	入力デバイスを抽象化して使うため起こすアクションの名前で設定する
+*/
+enum class GameKey
+{
+	Idle,
+	Walk,
+	Dash,
+	Crounch,
+	Jump,
+	Zoom,
+};
